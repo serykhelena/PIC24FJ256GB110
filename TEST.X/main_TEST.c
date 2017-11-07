81e83678930ba856c63439f7c8d7f028fd8de020
serykhelena <seryhelena@gmail.com>
2017-11-07 18:00:59 +0300

    Add ADC, Timer, UART(not working properly)

--- TEST.X/main_TEST.c
+++ TEST.X/main_TEST.c
@@ -1,31 +1,56 @@
 
 #include "pragma.h"
-
-#define FOSC        (32000000ULL)
-#define FCY         16000000
-
-#include "xc.h"
-#include <libpic30.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <math.h>
-#include <stdbool.h>
+#include "define.h"
 
 
+#include "timer.h"
+#include "adc.h"
+#include "uart.h"
+
+int counter = 0;
+int flag = 0;
+
+void __attribute__ ((interrupt, auto_psv)) _T1Interrupt(void) //прерывание по таймеру 1 мс
+{
+    counter = counter + 1;
+    
+    if(counter == 1)
+    {
+        LATEbits.LATE9 = 1;
+    }
+    if(counter == 2)
+    {
+        flag = read_ADC(11);
+    }
+    if(counter == 3)
+    {
+        LATEbits.LATE9 = 0;
+    }
+    if(counter == 6)
+    {
+        counter = 0;
+    }
+    
+     
+    IFS0bits.T1IF = 0;                   //Очищаем флаг по прерыванию таймера 1
+}
 
 
 int main(void) {
+//    init_ADC();
+//    init_timer1();
+    init_UART1();
+    
+    TRISBbits.TRISB4 = 1;               //RB4 = вход для приёма (RX)
+    TRISBbits.TRISB5 = 0;               //RB5 = выход для передачи (TX)     
+    RPINR18bits.U1RXR = 28;             //назначaeм RP24 для приёма данных (RX)   
+    RPOR9bits.RP18R = 3;                //назначаем RP18 для передачи данных (TX)
+    
     
     TRISEbits.TRISE9 = 0;       // set pin as output
     
-    while(1) 
-    {
-        LATEbits.LATE9 = 1;
-        __delay_ms(1000); 
-        LATEbits.LATE9 = 0;
-        __delay_ms(1000);
-        
-    }
+    while(1);
+    
+    
     return 0;
 }
